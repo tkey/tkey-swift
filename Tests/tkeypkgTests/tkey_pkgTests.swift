8519a5ac01f22ea11d7c7b3b41a08121c62880fe
shubham <16849485+metallicalfa2@users.noreply.github.com>
2023-02-23 16:16:51 +0800

    feat: updates

--- Tests/tkeypkgTests/tkey_pkgTests.swift
+++ Tests/tkeypkgTests/tkey_pkgTests.swift
@@ -2,12 +2,21 @@ import XCTest
 import Foundation
 @testable import tkey_pkg
 
+final class parenet: XCTestSuite{
+    
+}
+
 final class tkey_pkgTests: XCTestCase {
     func testLibraryVersion() {
         _ = try! library_version()
     }
     
-    func testGenerateDeleteShare() async {
+    func testGenerateAndDeleteShares() async {
+        await generateDeleteShare(true);
+        await generateDeleteShare(false);
+    }
+    
+    func generateDeleteShare(_ manual_sync: Bool) async {
         let storage_layer = try! StorageLayer(enable_logging: true, host_url: "https://metadata.tor.us", server_time_offset: 2)
         let key1 = try! PrivateKey.generate()
         let service_provider = try! ServiceProvider(enable_logging: true, postbox_key: key1.hex)
@@ -15,7 +24,7 @@ final class tkey_pkgTests: XCTestCase {
             storage_layer: storage_layer,
             service_provider: service_provider,
             enable_logging: true,
-            manual_sync: false)
+            manual_sync: manual_sync)
 
         _ = try! await threshold_key.initialize(never_initialize_new_key: false, include_local_metadata_transitions: false)
         let key_details = try! threshold_key.get_key_details()
@@ -34,7 +43,7 @@ final class tkey_pkgTests: XCTestCase {
             
         // create one more new shares
         let new_share = try! await threshold_key.generate_new_share()
-        let key_details_2 = try! threshold_key.get_key_details()
+//        let key_details_2 = try! threshold_key.get_key_details()
         let share_index = new_share.hex;
             
         // await for previous promise to resolve
@@ -55,6 +64,7 @@ final class tkey_pkgTests: XCTestCase {
         }catch {
          // Should throw error
         }
+        
     }
 
     func testThresholdInputOutputShare() async {
